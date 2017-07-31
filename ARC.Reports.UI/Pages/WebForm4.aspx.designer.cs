80d8a31ec3e4814aad2c82e80b85d5dcce7fe823
Emad Nasser <emadnasser@msn.com>
2017-07-31 09:00:28 +0300

    2017.07.31

--- ARC.Reports.UI/Pages/WebForm4.aspx.designer.cs
+++ ARC.Reports.UI/Pages/WebForm4.aspx.designer.cs
@@ -13,15 +13,6 @@ namespace ARC.Reports.Pages {
     public partial class WebForm4 {
         
         /// <summary>
-        /// WebChartControl1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::DevExpress.XtraCharts.Web.WebChartControl WebChartControl1;
-        
-        /// <summary>
         /// WebChartControl2 control.
         /// </summary>
         /// <remarks>
