e7b98c9f6ad1055f59cbf0f9e59ee676e4f4a01c
Emad Nasser <emadnasser@msn.com>
2017-10-12 14:52:06 +0300

    2017.10.12

---
+++ DX_Bootstrapped/Controllers/PagesController.MarketShareDetails.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DX_Bootstrapped.Controllers
+{
+    public partial class PagesController : Controller
+    {
+        public ActionResult MarketShareDetails()
+        {
+            return View();
+        }
+    }
+}
