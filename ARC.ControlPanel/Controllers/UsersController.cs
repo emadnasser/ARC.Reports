d2a997de33c09a0ccf0dc054ec8659c7c4f8401c
Emad Nasser <emadnasser@msn.com>
2017-11-15 13:33:45 +0300

    2017.11.15

--- ARC.ControlPanel/Controllers/UsersController.cs
+++ ARC.ControlPanel/Controllers/UsersController.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using ARC.ControlPanel.Models;
+using System;
 using System.Collections.Generic;
+using System.DirectoryServices;
 using System.DirectoryServices.AccountManagement;
 using System.Linq;
 using System.Web;
@@ -10,40 +12,55 @@ namespace ARC.ControlPanel.Controllers
     public class UsersController : Controller
     {
         // GET: Users
-        //[HttpPost]
-        public ActionResult Index(string txtAmount)
+        public ActionResult Index()
         {
-            using (PrincipalContext ctx = new PrincipalContext(ContextType.Domain, "alrajhi.bank", "0100589", "uVZd0456"))
+            User user = new User();
+
+            try
             {
-                string strName = "";
-                if (Request["txtAmount"] != null )
-                    strName = Request["txtAmount"].ToString();
-
-                //string strName2 = form["txtId"].ToString();
-                //string strName3 = Convert.ToString(txtId);
-
-                UserPrincipal user = UserPrincipal.FindByIdentity(ctx, "0100589");
-
-                if (user != null)
+                using (PrincipalContext principalContext = new PrincipalContext(ContextType.Domain, "alrajhi.bank", "0100589", "uVZd0456"))
                 {
-                    // do something here....
+                    string txtId = Request["txtId"].ToString().Trim();
+                    //string txtName = Request["txtName"].ToString();
+
+                    UserPrincipal _userPrincipal = UserPrincipal.FindByIdentity(principalContext, "0100589");
+                    UserPrincipal userPrincipal = UserPrincipal.FindByIdentity(principalContext, txtId);
+
+                    if (userPrincipal != null && !_userPrincipal.IsAccountLockedOut() && !userPrincipal.IsAccountLockedOut())
+                    {
+                        DirectoryEntry directoryEntry = userPrincipal.GetUnderlyingObject() as DirectoryEntry;
+
+                        if (directoryEntry.Properties["samAccountName"].Value != null)
+                            user.UserId = directoryEntry.Properties["samAccountName"].Value.ToString();
+
+                        if (directoryEntry.Properties["givenName"].Value != null)
+                            user.FirstName = directoryEntry.Properties["givenName"].Value.ToString();
+
+                        if (directoryEntry.Properties["sn"].Value != null)
+                            user.LastName = directoryEntry.Properties["sn"].Value.ToString();
+
+                        if (directoryEntry.Properties["mail"].Value != null)
+                            user.Email = directoryEntry.Properties["mail"].Value.ToString();
+
+                        if (directoryEntry.Properties["TelephoneNumber"].Value != null)
+                            user.Phone = directoryEntry.Properties["TelephoneNumber"].Value.ToString();
+
+                        if (directoryEntry.Properties["mobile"].Value != null)
+                            user.Mobile = directoryEntry.Properties["mobile"].Value.ToString();
+
+                        if (directoryEntry.Properties["department"].Value != null)
+                            user.Department = directoryEntry.Properties["department"].Value.ToString();
+
+                        if (directoryEntry.Properties["title"].Value != null)
+                            user.JobTitle = directoryEntry.Properties["title"].Value.ToString();
+                    }
                 }
-
-
-                // or alternatively: get the currently logged in user
-                //UserPrincipal current = UserPrincipal.Current;
-
-
-                //return user.Surname;
-                return View();
-
+            }
+            catch
+            {
             }
 
-            //EmployeeContext employeeContext = new EmployeeContext();
-            //Employee employee = employeeContext.Employees.Single(x => x.EmployeeId == id);
-
-            //return View(employee);
-
+            return View(user);
         }
 
         // GET: Users/Details/5
---
+++ ARC.ControlPanel/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ARC.ControlPanel.Models
+{
+    public class User
+    {
+        public String UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Mobile { get; set; }
+        public string Department { get; set; }
+        public string JobTitle { get; set; }
+    }
+}
